e5dcb297ebc21f784b6bd5d2a8195a686ce950e1
1grey1 <125741721+1grey1@users.noreply.github.com>
2023-06-03 21:32:52 +0300

    Add files via upload

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,41 +1,29 @@
-let number1 = prompt('Введите первый операнд (число):');
-let operator = prompt('Введите оператор: "+"; "-"; "*"; "/":');
-let number2 = prompt('Введите второй операнд (число):');
+function calc() {
+    const number1 = prompt('Введите первый операнд (число):');
+    const operator = prompt('Введите оператор: "+"; "-"; "*"; "/":');
+    const number2 = prompt('Введите второй операнд (число):');
 
-function sum(value) {
-    return 'Сложение: ' + value;
+    if (operator === '+') {
+        const stringSum = Number(number1) + Number(number2);
+        console.log('Ответ: ' + stringSum);
+    }
+    
+    if (operator === '-') {
+        const stringSubtraction = Number(number1) - Number(number2);
+        console.log('Ответ: ' + stringSubtraction);
+    }
+    
+    if (operator === '*') {
+        const stringMultiplication = Number(number1) * Number(number2);
+        console.log('Ответ: ' + stringMultiplication);
+    }
+    
+    if (operator === '/' && number2 !== '0') {
+        const stringDivision = Number(number1) / Number(number2);
+        console.log('Ответ: ' + stringDivision);
+    } else if (operator === '/' && number2 === '0') {
+        console.log('Ошибка в вычислении');
+    }
 }
 
-function subtraction(value) {
-    return 'Вычетание: ' + value;
-}
-
-function multiplication(value) {
-    return 'Умножение: ' + value;
-}
-
-function division(value) {
-    return 'Деление: ' + value;
-}
-
-if (operator === '+') {
-    const stringSum = sum(Number(number1) + Number(number2));
-    console.log(stringSum);
-}
-
-if (operator === '-') {
-    const stringSubtraction = subtraction(Number(number1) - Number(number2));
-    console.log(stringSubtraction);
-}
-
-if (operator === '*') {
-    const stringMultiplication = multiplication(Number(number1) * Number(number2));
-    console.log(stringMultiplication);
-}
-
-if (operator === '/' && number2 !== '0') {
-    const stringDivision = division(Number(number1) / Number(number2));
-    console.log(stringDivision);
-} else if (operator === '/' && number2 === '0') {
-    console.log('Ошибка в вычислении');
-}
+calc();
