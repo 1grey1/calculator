5d418f2c6783d4d8b80d8162c6478d366fa0857f
1grey1 <125741721+1grey1@users.noreply.github.com>
2023-06-05 20:05:45 +0300

    Add files via upload

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,28 +1,29 @@
+const number1 = prompt('Введите первый операнд (число):');
+const operator = prompt('Введите оператор: "+"; "-"; "*"; "/":');
+const number2 = prompt('Введите второй операнд (число):');
+
 function calc() {
-    const number1 = prompt('Введите первый операнд (число):');
-    const operator = prompt('Введите оператор: "+"; "-"; "*"; "/":');
-    const number2 = prompt('Введите второй операнд (число):');
 
     if (operator === '+') {
-        const stringSum = Number(number1) + Number(number2);
-        console.log('Ответ: ' + stringSum);
+        const stringSum = Number(number1 + number2);
+        return alert('Ответ: ' + stringSum);
     }
     
     if (operator === '-') {
-        const stringSubtraction = Number(number1) - Number(number2);
-        console.log('Ответ: ' + stringSubtraction);
+        const stringSubtraction = Number(number1 - number2);
+        return alert('Ответ: ' + stringSubtraction);
     }
     
     if (operator === '*') {
-        const stringMultiplication = Number(number1) * Number(number2);
-        console.log('Ответ: ' + stringMultiplication);
+        const stringMultiplication = Number(number1 * number2);
+        return alert('Ответ: ' + stringMultiplication);
     }
     
     if (operator === '/' && number2 !== '0') {
-        const stringDivision = Number(number1) / Number(number2);
-        console.log('Ответ: ' + stringDivision);
+        const stringDivision = Number(number1 / number2);
+        return alert('Ответ: ' + stringDivision);
     } else if (operator === '/' && number2 === '0') {
-        console.log('Ошибка в вычислении');
+        return 'Ошибка в вычислении';
     }
 }
 
