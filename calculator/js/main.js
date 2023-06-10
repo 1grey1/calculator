4c44543e194fecd5134cc14d989d8325c47dc0e2
1grey1 <125741721+1grey1@users.noreply.github.com>
2023-06-10 15:38:08 +0300

    Delete calculator directory

--- calculator/js/main.js
+++
@@ -1,6 +0,0 @@
-const number1 = Number(prompt('Введите первый операнд (число):'));
-const operator = prompt('Введите оператор: "+"; "-"; "*"; "/":');
-const number2 = Number(prompt('Введите второй операнд (число):'));
-
-const result = calc(number1, operator, number2);
-alert(result);
