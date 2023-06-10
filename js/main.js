b5e87b1a0f1d832d351356d8b3b92f5b8e66e885
1grey1 <125741721+1grey1@users.noreply.github.com>
2023-06-10 17:16:04 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const number1 = Number(prompt('Введите первый операнд (число):'));
-const operator = prompt('Введите оператор: "+"; "-"; "*"; "/":');
-const number2 = Number(prompt('Введите второй операнд (число):'));
+let reset = confirm('Начать решение?');
 
-const result = calc(number1, operator, number2);
-alert(result);
+while (reset != false) {
+    const number1 = Number(prompt('Введите первый операнд (число):'));
+    const operator = prompt('Введите оператор: "+"; "-"; "*"; "/":');
+    const number2 = Number(prompt('Введите второй операнд (число):'));
+    
+    const result = calc(number1, operator, number2);
+    alert('Ответ: ' + result);
+
+    reset = confirm('Хотите ли повторить действие?');
+}
